e5c30564773822fd47cede757dc19695322ec5c8
Rephi <83082012+Rephidock@users.noreply.github.com>
2023-09-18 16:23:55 +0300

    Create BackendProcessException.cs

---
+++ Front/ConBridge/BackendProcessException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace AlbaLilium.Nliois.ConBridge;
+
+
+internal class BackendProcessException : Exception {
+
+	public BackendProcessException() : base() { }
+
+	public BackendProcessException(string? message) : base(message) {}
+
+	public BackendProcessException(string? message, Exception? innerException) : base(message, innerException) {}
+
+}

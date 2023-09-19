6d526c3b6b452332c40723de6c3034c461c36fee
Rephi <83082012+Rephidock@users.noreply.github.com>
2023-09-19 19:16:09 +0300

    Create QueryResult.cs

---
+++ Front/ConBridge/QueryResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace AlbaLilium.Nliois.ConBridge;
+
+
+public record QueryResult<T> {
+
+	/// <summary>True of query was processed without errors</summary>
+	public required bool IsSuccess { get; init; }
+
+	/// <summary>Error message if there was an error. Null for success.</summary>
+	public string? ErrorMessage { get; init; } = null;
+
+	/// <summary>Response to the query. Default (may be null) for error.</summary>
+	public T? Response { get; init; } = default;
+
+}
